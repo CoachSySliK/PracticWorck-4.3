ecd3851d24f007ee4a5b765d7df6e3a490629cb7
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-05 22:30:56 +0300

    Добавьте файлы проекта.

---
+++ PracticWorck-4.3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Проверка на чётное число\n\n";
+    cout << "Введите число: ";
+    cin >> x;
+    if (0 != x % 2) {
+        cout << "Число " << x << " не чётное!\n";
+    }
+    else {
+        cout << "Число " << x << " чётное!\n";
+    }
+    return 0;
+
+
+}
